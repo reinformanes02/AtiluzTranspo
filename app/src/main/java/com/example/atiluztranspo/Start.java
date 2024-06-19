f216dd43f1b81bba33a322d7eb0b1fdfee87222a
reinformanes02 <162852905+reinformanes02@users.noreply.github.com>
2024-06-19 22:49:57 +0800

    file

--- app/src/main/java/com/example/atiluztranspo/Start.java
+++ app/src/main/java/com/example/atiluztranspo/Start.java
@@ -1,26 +1,44 @@
 package com.example.atiluztranspo;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Spinner;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Start extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_start);
+
         Button exitButton = findViewById(R.id.exitButton);
         exitButton.setOnClickListener(view -> {
             // Close the current activity
             finishAffinity(); // This will close all activities and exit the application
             // System.exit(0); // Optional: this can be used to forcefully exit the app
+        });
+
+        Spinner roleSpinner = findViewById(R.id.roleSpinner);
+        roleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedRole = parent.getItemAtPosition(position).toString();
+                if (selectedRole.equals("Driver")) {
+                    Intent intent = new Intent(Start.this, DriverLogs.class);
+                    startActivity(intent);
+                } else if (selectedRole.equals("Passenger")) {
+                    Intent intent = new Intent(Start.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
         });
     }
 }
